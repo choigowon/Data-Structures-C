0e1b6e7fa46b278836a1fa289953ca8b3bf508ae
최고원 <154347680+choigowon@users.noreply.github.com>
2025-01-02 22:54:52 +0900

    Chapter 5 - 이중 연결 리스트(맨 앞 노드 삽입)

--- doubly_linked_list.c
+++ doubly_linked_list.c
@@ -8,3 +8,17 @@ typedef struct node { // 이중 연결 리스트 노드
 }node;
 
 node* head = NULL;
+node* tail = NULL;
+
+void insert(int data) { // 맨 앞에 노드 삽입
+	node* new = (node*)malloc(sizeof(node));
+	new->value = data;
+	new->prev = new->next = NULL;
+	if (head = NULL) {
+		head = tail = new;
+		return;
+	}
+	new->next = head;
+	head->prev = new;
+	head = new;
+}
