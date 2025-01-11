14a8d636e8ed0edfee0ea5c9410942156dcdc65c
최고원 <154347680+choigowon@users.noreply.github.com>
2025-01-11 21:58:19 +0900

    Chapter 8 - 재귀 호출(거듭 제곱)

--- recursive_call.c
+++ recursive_call.c
@@ -8,7 +8,14 @@ void recursive(int n) {
 	printf("%d ", n); // 재귀 호출 이후에 실행되므로 결과는 역순
 }
 
+int power(int x, int y) { // x의 y 거듭제곱
+	if (y == 0)
+		return 1;
+	return x * power(x, y - 1);
+}
+
 int main() {
 	recursive(3);
+	printf("\n%d\n", power(2, 3));
 	return 0;
 }
