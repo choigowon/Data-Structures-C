423228d803d0b83a7efa79c9679c96a3595d6baa
최고원 <154347680+choigowon@users.noreply.github.com>
2025-01-13 17:57:07 +0900

    Chapter 9 - 삽입 정렬

--- sort.c
+++ sort.c
@@ -28,6 +28,17 @@ void selectionSort(int* arr, int size) {
 	}
 }
 
+void insertionSort(int* arr, int size) {
+	int i, j, temp;
+	for (i = 1; i < size; i++) {
+		temp = arr[i];
+		for (j = i; j > 0 && arr[j - 1] > temp; j--) {
+			arr[j] = arr[j - 1];
+		}
+		arr[j] = temp;
+	}
+}
+
 int main() {
 
 }
